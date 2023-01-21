9bdf7ccb4f8d4f029613c367a96657424ca77fe5
bilal-ahmed89 <ba1770490@gmail.com>
2023-01-21 15:57:12 +0500

    modified

--- src/components/CategoriesCard/CategoriesCard.jsx
+++ src/components/CategoriesCard/CategoriesCard.jsx
@@ -11,7 +11,7 @@ function CategoriesCard({ categories }) {
                     return (
                         <div className="col-md-3 mb-4">
                             <div className={`${styles.categoriesCard}`}>
-                                <img src={kruchBurger} alt="" style={{ width: '200px', borderRadius: '6px', border: '1px dashed white' }} />
+                                <img src={kruchBurger} alt="" style={{ width: '180px', borderRadius: '6px', border: '1px dashed white',height : '170px' }} />
 
                                 <span className={`position-absolute start-50 translate-middle-x ${styles.categoriesTitle} `}>{category.title}</span>
                             </div>
