6ef754f0437e90fa1c7a519c1ac14359a6ef5ec8
bilal-ahmed89 <ba1770490@gmail.com>
2023-01-21 15:44:23 +0500

    modified

---
+++ src/components/CategoriesCard/CategoriesCard.jsx
@@ -0,0 +1,27 @@
+import styles from './CategoriesCard.module.css'
+import kruchBurger from '../../assets/imgs/krunchBurger.jpg'
+import krunchBurger from '../../assets/imgs/krunchBurger.jpg'
+
+
+function CategoriesCard({ categories }) {
+    return (
+        <div className="row">
+            {
+                categories.map((category) => {
+                    return (
+                        <div className="col-md-3 mb-4">
+                            <div className={`${styles.categoriesCard}`}>
+                                <img src={kruchBurger} alt="" style={{ width: '200px', borderRadius: '6px', border: '1px dashed white' }} />
+
+                                <span className={`position-absolute start-50 translate-middle-x ${styles.categoriesTitle} `}>{category.title}</span>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+
+        </div>
+
+    );
+}
+export default CategoriesCard;
---
+++ src/components/data/data.js
@@ -0,0 +1,27 @@
+
+
+
+
+export const categories = [
+    {
+        id : 1,
+        title : 'EVERYDAY VALUE',
+        img : '../../assets/img/krunchBurger'
+    },
+    {
+        id : 2,
+        title : 'AL CARTE & COMBOS',
+        img : "./assets/imgs/alaCrateCombo.jpg"
+    },
+    {
+        id : 3,
+        title : 'SIGNATURE BOXES',
+        img : "./assets/imgs/alaCrateCombo.jpg"
+    },
+    {
+        id : 4,
+        title : 'SHARING',
+        img : "./assets/imgs/alaCrateCombo.jpg"
+    },
+    
+]
